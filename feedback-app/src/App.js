9d9b98d8a0a0cd0b7e5fe2460d9846e00bb0f0a5
christopherdec <christopher.c.c@hotmail.com>
2022-09-04 11:29:06 -0300

    state and useSate hook

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,34 +1,15 @@
+import FeedbackItem from "./components/FeedbackItem"
+import Header from "./components/Header"
+
 const App = () => {
-  const title = "Blog Post"
-  const body = "This is my blog"
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three" },
-  ]
-
-  const loading = true
-  const showComments = true
-
-  const commentBlock = (
-    <div className='comments'>
-          <h3>Comments ({comments.length})</h3>
-          <ul>
-            {comments.map((comment, index) => (
-              <li key={index}>{comment.text}</li>
-            ))}
-          </ul>
-        </div>
-  )
-
-  return (
-    <div className='container'>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-    </div>
-  )
+    return (
+        <>
+            <Header />
+            <div className='container'>
+                <FeedbackItem />
+            </div>
+        </>
+    )
 }
 
 export default App
