fdb0be5e47f83ffbcd267141bd5619a1d4799aff
christopherdec <christopher.c.c@hotmail.com>
2022-09-04 10:45:06 -0300

    react baiscs and jsx - blog post example

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const title = "Blog Post"
+  const body = "This is my blog"
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ]
 
-function App() {
+  const loading = true
+  const showComments = true
+
+  const commentBlock = (
+    <div className='comments'>
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment.text}</li>
+            ))}
+          </ul>
+        </div>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
