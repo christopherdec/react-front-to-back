e5309bc234e13ec6cc15d8d917adf09e52d20fdb
christopherdec <christopher.c.c@hotmail.com>
2022-09-05 08:32:40 -0300

    added eventos and prop drilling

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,12 +1,24 @@
-import FeedbackItem from "./components/FeedbackItem"
+import { useState } from "react"
+import FeedbackList from "./components/FeedbackList"
 import Header from "./components/Header"
+import FeedbackData from "./data/FeedbackData"
+
 
 const App = () => {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return (
         <>
             <Header />
             <div className='container'>
-                <FeedbackItem />
+                <FeedbackList feedback={feedback}
+                    handleDelete={deleteFeedback} />
             </div>
         </>
     )
--- feedback-app/src/index.js
+++ feedback-app/src/index.js
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-createRoot(document.getElementById('root'))
-    .render(<App />)
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
