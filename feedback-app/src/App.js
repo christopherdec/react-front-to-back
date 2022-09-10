7b8b35cea85d2f1c44b8ab4764661ae045c0d520
christopherdec <christopher.c.c@hotmail.com>
2022-09-10 13:40:35 -0300

    finished section 5 - added route to aboutpage

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,17 +1,20 @@
 import { useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid'
+import AboutIconLink from "./components/AboutIconLink"
 import FeedbackForm from "./components/FeedbackForm"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import Header from "./components/Header"
 import FeedbackData from "./data/FeedbackData"
+import AboutPage from "./pages/AboutPage"
 
 
 const App = () => {
     const [feedback, setFeedback] = useState(FeedbackData)
 
     const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete?')) {
+        if (window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
@@ -22,15 +25,24 @@ const App = () => {
     }
 
     return (
-        <>
+        <Router>
             <Header />
             <div className='container'>
-                <FeedbackForm handleAdd= {addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback}
-                    handleDelete={deleteFeedback} />
+                <Routes>
+                    <Route path='/' exact element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList feedback={feedback}
+                                handleDelete={deleteFeedback} />
+                            <AboutIconLink />
+                        </>
+                    } />
+                    <Route path="/about" element={<AboutPage />} />
+                </Routes>
             </div>
-        </>
+        </Router>
     )
 }
 
