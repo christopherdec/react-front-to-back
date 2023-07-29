53cdbea66d3a8399c8b02258276d4ba8db1764a8
Christopher <christopher.c.c@hotmail.com>
2023-07-29 12:08:05 -0300

    failed to fix Swiper component, finishing project

--- house-marketplace/src/pages/Listing.jsx
+++ house-marketplace/src/pages/Listing.jsx
@@ -1,101 +1,142 @@
-import React, { useState, useEffect } from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.conf'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-
-function formatPrice(price) {
-  if (!price) return '';
-  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
+  const [listing, setListing] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [shareLinkCopied, setShareLinkCopied] = useState(false)
 
-  const [listing, setListing] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [shareLinkCopied, setShareLinkCopied] = useState(false);
-
-  const navigate = useNavigate();
-  const params = useParams();
-  const auth = getAuth();
+  const navigate = useNavigate()
+  const params = useParams()
+  const auth = getAuth()
 
   useEffect(() => {
-    async function fetchListing() {
-      const docRef = doc(db, 'listings', params.listingId);
-      const docSnap = await getDoc(docRef);
+    const fetchListing = async () => {
+      const docRef = doc(db, 'listings', params.listingId)
+      const docSnap = await getDoc(docRef)
 
       if (docSnap.exists()) {
-        console.log(docSnap.data());
-        setListing(docSnap.data());
-        setLoading(false);
+        setListing(docSnap.data())
+        setLoading(false)
       }
     }
-    fetchListing();
+
+    fetchListing()
   }, [navigate, params.listingId])
 
-  if (loading) return <Spinner />;
+  if (loading) {
+    return <Spinner />
+  }
 
   return (
     <main>
-      <div className="shareIconDiv" onClick={() => {
-        navigator.clipboard.writeText(window.location.href);
-        setShareLinkCopied(true);
-        setTimeout(() => {
-          setShareLinkCopied(false);
-        }, 1337)
-      }}>
+      <Helmet>
+        <title>{listing.name}</title>
+      </Helmet>
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imageUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <div
+        className='shareIconDiv'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href)
+          setShareLinkCopied(true)
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, 2000)
+        }}
+      >
         <img src={shareIcon} alt='' />
       </div>
-      {shareLinkCopied && <p className='linkCopied'>{"Link copiado!"}</p>}
-      <div className="listingDetails">
-        <p className="listingName">{listing.name} - {formatPrice(listing.offer ? listing.discountedPrice : listing.regularPrice)}</p>
-        <p className="listingLocation">{listing.location}</p>
-        <p className="listingType">For {listing.type === 'rent' ? 'Rent' : 'Sale'}</p>
-        {listing.offer &&
-          <p className="discountPrice">
-            R${listing.regularPrice - listing.discountedPrice} discount
+
+      {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
+
+      <div className='listingDetails'>
+        <p className='listingName'>
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listingLocation'>{listing.location}</p>
+        <p className='listingType'>
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className='discountPrice'>
+            ${listing.regularPrice - listing.discountedPrice} discount
           </p>
-        }
+        )}
+
         <ul className='listingDetailsList'>
           <li>
-            {listing.bedrooms > 1 ? `${listing.bedrooms} quartos` : '1 quarto'}
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : '1 Bedroom'}
           </li>
           <li>
-            {listing.bathrooms > 1 ? `${listing.bathrooms} banheiros` : '1 banheiro'}
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : '1 Bathroom'}
           </li>
-          <li>
-            {listing.parking && 'Estacionamento'}
-          </li>
-          <li>
-            {listing.furnished && 'Mobiliado'}
-          </li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
         </ul>
-        <p className="listingLocationTitle">Location</p>
+
+        <p className='listingLocationTitle'>Location</p>
 
         <div className='leafletContainer'>
-          <MapContainer 
-            style={{ height: '100%', width: '100%' }} 
-            center={[ listing.geolocation.lat, listing.geolocation.lng ]}
+          <MapContainer
+            style={{ height: '100%', width: '100%' }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
             zoom={13}
             scrollWheelZoom={false}
           >
-            <TileLayer 
+            <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
             />
-            <Marker position={[ listing.geolocation.lat, listing.geolocation.lng ]}>
+
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
               <Popup>{listing.location}</Popup>
             </Marker>
           </MapContainer>
         </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
-          <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`}
-            className='primaryButton'>
-            Contatar vendedor
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className='primaryButton'
+          >
+            Contact Landlord
           </Link>
         )}
       </div>
@@ -104,3 +145,5 @@ function Listing() {
 }
 
 export default Listing
+
+// https://stackoverflow.com/questions/67552020/how-to-fix-error-failed-to-compile-node-modules-react-leaflet-core-esm-pat
