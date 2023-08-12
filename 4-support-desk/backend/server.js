7f52fcab849e3eabd7c0e90e1342bef322e6c806
Christopher <christopher.c.c@hotmail.com>
2023-08-12 17:57:01 -0300

    Prepared for deploy on Heroku

--- 4-support-desk/backend/server.js
+++ 4-support-desk/backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const { errorHandler } = require('./middleware/errorMiddleware')
@@ -12,13 +13,23 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support Desk API' })
-})
-
 app.use('/api/users', require('./routes/userRoutes'))
-
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// serve the frontend
+if (process.env.NODE_ENV === 'production') {
+
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  // routes everthing else
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+
+} else {
+  
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
+}
 
 app.use(errorHandler)
 
